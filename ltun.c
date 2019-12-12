36ebc42fa615b6580fe496056d213be03f3e8892
Chen Minqiang <ptpt52@gmail.com>
2019-12-12 23:34:19 +0800

    kcp fast3

--- ltun.c
+++ ltun.c
@@ -768,10 +768,12 @@ rawkcp_t *new_rawkcp(unsigned int conv, const unsigned char *remote_id)
 	rkcp->buf->idx = 0;
 
 	rkcp->kcp->output = rawkcp_output;
-	ikcp_wndsize(rkcp->kcp, 256, 384);
-	ikcp_nodelay(rkcp->kcp, 0, 10, 0, 0);
+	ikcp_wndsize(rkcp->kcp, 512, 768);
+	ikcp_nodelay(rkcp->kcp, 1, 10, 2, 1);
+	rkcp->kcp->rx_minrto = 10;
+	rkcp->kcp->fastresend = 1;
 
-	rkcp->kcp_max_poll = 384 * rkcp->kcp->mss / BUF_SIZE / 4;
+	rkcp->kcp_max_poll = 512 * rkcp->kcp->mss / BUF_SIZE / 2;
 
 	ev_timer_init(&rkcp->watcher, rawkcp_watcher_cb, 0.1, 0.01);
 
